9497054ff1aa5785f5d48c74f91ecafdba79d382
Dying-fish <2011710343@qq.com>
2020-06-03 23:16:06 +0800

    information

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'TestUser');
+define('DBPASS', 'test_web_lab');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
